7b5e3a6431882277c4bb7415d1d4f08ecf967125
fazalshahzad <shancompany322@gmail.com>
2022-05-25 14:53:31 +0500

    commit

---
+++ src/app/header/header.js
@@ -0,0 +1,7 @@
+const bar = document.getElementById('bar')
+const nav = document.getElementById('nav')
+if(bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
